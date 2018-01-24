d1e28a8242d7ac3084ab78268474e8035eed9582
Sagar Makhar <sagarmakhar@gmail.com>
2018-01-24 17:08:33 +0000

    s2:Fix Audio and add custom audio policy

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -17,6 +17,7 @@
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
