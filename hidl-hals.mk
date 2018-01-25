6393437755cef6e170fceccdae977c336416c938
Renji <vishwakarmanikhil18@gmail.com>
2018-01-25 08:44:41 +0000

    added biometrics

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -54,7 +54,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service-custom
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
